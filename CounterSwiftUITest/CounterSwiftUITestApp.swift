d55ac2b4062a7c6b8ff130f0e13d79e2f763d7f9
Bellaaspera <lanasenatorova@gmail.com>
2023-07-05 21:15:10 +0300

    Initial Commit

---
+++ CounterSwiftUITest/CounterSwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterSwiftUITestApp.swift
+//  CounterSwiftUITest
+//
+//  Created by Светлана Сенаторова on 05.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterSwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
